4bcd65c34a85eb3d7fc8866f6966bf45292cd7b8
Anurag Patil <41022671+patilanurag@users.noreply.github.com>
2022-09-24 16:52:20 -0400

    Latest Updates

--- Client-Server-Actor-Model/client.erl
+++ Client-Server-Actor-Model/client.erl
@@ -1,5 +1,4 @@
 -module(client).
--import(string, [substr/3, equal/2, len/1]).
 -export([beginWork/2]).
 
 beginWork(MID, Zs) ->
@@ -19,16 +18,10 @@ findHash(S, E, MID, Z) ->
 
 checkString(N, Z, MID) ->
     BitcoinKey = "a.patil:" ++ integer_to_list(N),
-    % SHA = binary:decode_unsigned(crypto:hash(sha256, BitcoinKey)),
-    SHA_String = io_lib:format("~64.16.0b", [
-        binary:decode_unsigned(crypto:hash(sha256, BitcoinKey))
-    ]),
-    Subs = substr(SHA_String, 1, Z),
-    ZeroString = "00000000000000000000000000000000000000000000000000000000000",
-    ZeroSubs = substr(ZeroString, 1, Z),
-    case equal(Subs, ZeroSubs) of
+    <<BKey:256>> = crypto:hash(sha256, BitcoinKey),
+    SHA_String = io_lib:format("~64.16.0b", [BKey]),
+    case (string:substr(SHA_String, 1, Z) == string:left("", Z, $0)) of
         true ->
-            % io:fwrite("~p       ~p     ~s       ~p~n", [self(), BitcoinKey, SHA_String, MID]);
             MID ! {self(), {BitcoinKey, SHA_String}};
         false ->
             done
--- Client-Server-Actor-Model/server.erl
+++ Client-Server-Actor-Model/server.erl
@@ -1,24 +1,22 @@
 -module(server).
 -import(math, [pow/2]).
-% -import(actors, [beginWork/2]).
 -import(client, [beginWork/2]).
 -export([main/1]).
 
 main(Noz) ->
     % Number of actors, to be selected at random
     RandN = 5,
-    % {ok, [Noz]} = io:fread("input : ", "~d"),
     MasterID = spawn(fun() -> endlessLoop(RandN) end),
     generateActors(MasterID, Noz, RandN).
 
 endlessLoop(Rn) ->
     receive
         {AID, {range}} ->
-            End = Rn + round(pow(10, 6)),
+            End = Rn + 1000000000,
             AID ! {self(), {Rn, End}},
-            endlessLoop(End + round(pow(10, 6)));
+            endlessLoop(End + 1000000000);
         {AID, {BTC, SHA_String}} ->
-            io:fwrite("Actor ID: ~p Output: ~p  ~p~n", [AID, BTC, SHA_String]),
+            io:format("Actor ID: ~p Output: ~p  ~p~n", [AID, BTC, SHA_String]),
             endlessLoop(Rn)
     end.
 
@@ -28,5 +26,5 @@ generateActors(MID, Zs, Rn) ->
         true ->
             generateActors(MID, Zs, Rn - 1);
         false ->
-            ok
+            done
     end.
